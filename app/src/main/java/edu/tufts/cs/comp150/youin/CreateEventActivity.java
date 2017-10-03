00dee08444268f5eea02a32bf3580ae4f138dbed
Frank Hattler <frank.hattler@tufts.edu>
2017-10-03 15:40:01 -0400

    added functionality to what and when tabs for the create event view

--- app/src/main/java/edu/tufts/cs/comp150/youin/CreateEventActivity.java
+++ app/src/main/java/edu/tufts/cs/comp150/youin/CreateEventActivity.java
@@ -1,14 +1,78 @@
 package edu.tufts.cs.comp150.youin;
 
-import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
 
 public class CreateEventActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
     }
+
+    public void navigateToWhat(View v) {
+        EditText eventName = (EditText)findViewById(R.id.eventName);
+        EditText eventDescription = (EditText)findViewById(R.id.eventDescription);
+
+        eventName.setVisibility(View.VISIBLE);
+        eventDescription.setVisibility(View.VISIBLE);
+
+        TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
+        DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
+
+        timePicker.setVisibility(View.GONE);
+        datePicker.setVisibility(View.GONE);
+
+        Button what = (Button)findViewById(R.id.createEventWhatButton);
+        Button when = (Button)findViewById(R.id.createEventWhenButton);
+        what.setBackgroundColor(getResources().getColor(R.color.blue));
+        when.setBackgroundColor(getResources().getColor(R.color.white));
+
+    }
+
+    public void navigateToWhen(View v) {
+        EditText eventName = (EditText)findViewById(R.id.eventName);
+        EditText eventDescription = (EditText)findViewById(R.id.eventDescription);
+
+        eventName.setVisibility(View.GONE);
+        eventDescription.setVisibility(View.GONE);
+
+        TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
+        DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
+
+        timePicker.setVisibility(View.VISIBLE);
+        datePicker.setVisibility(View.VISIBLE);
+
+        Button what = (Button)findViewById(R.id.createEventWhatButton);
+        Button when = (Button)findViewById(R.id.createEventWhenButton);
+        what.setBackgroundColor(getResources().getColor(R.color.white));
+        when.setBackgroundColor(getResources().getColor(R.color.blue));
+
+    }
+
+    /*
+    public void navigateEventCreation(View v) {
+        EditText eventName = (EditText)findViewById(R.id.eventName);
+        EditText eventDescription = (EditText)findViewById(R.id.eventDescription);
+        eventName.setVisibility(View.GONE);
+        eventDescription.setVisibility(View.GONE);
+
+        TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
+        DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
+        timePicker.setVisibility(View.VISIBLE);
+        datePicker.setVisibility(View.VISIBLE);
+
+        Button what = (Button)findViewById(R.id.createEventWhatButton);
+        Button when = (Button)findViewById(R.id.createEventWhenButton);
+        what.setBackgroundColor(getResources().getColor(R.color.white));
+        when.setBackgroundColor(getResources().getColor(R.color.blue));
+
+    }*/
 }
