d2efdd356bdf88a307c86d9138961b910edc83db
Frank Hattler <frank.hattler@tufts.edu>
2017-10-03 20:42:21 -0400

    implemented basic navigation and layout for the create event activity

--- app/src/main/java/edu/tufts/cs/comp150/youin/CreateEventActivity.java
+++ app/src/main/java/edu/tufts/cs/comp150/youin/CreateEventActivity.java
@@ -2,11 +2,11 @@ package edu.tufts.cs.comp150.youin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TimePicker;
+import android.widget.ListView;
 
 
 public class CreateEventActivity extends AppCompatActivity {
@@ -15,64 +15,68 @@ public class CreateEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        //test populate the list view
+        String[] myStringArray = {"Frank","Frances","Fernando"};
+        ArrayAdapter<String> testAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, myStringArray);
+        ListView friendsList = (ListView) findViewById(R.id.inviteList);
+        friendsList.setAdapter(testAdapter);
     }
 
     public void navigateToWhat(View v) {
-        EditText eventName = (EditText)findViewById(R.id.eventName);
-        EditText eventDescription = (EditText)findViewById(R.id.eventDescription);
-
-        eventName.setVisibility(View.VISIBLE);
-        eventDescription.setVisibility(View.VISIBLE);
-
-        TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
-        DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
-
-        timePicker.setVisibility(View.GONE);
-        datePicker.setVisibility(View.GONE);
-
-        Button what = (Button)findViewById(R.id.createEventWhatButton);
-        Button when = (Button)findViewById(R.id.createEventWhenButton);
-        what.setBackgroundColor(getResources().getColor(R.color.blue));
-        when.setBackgroundColor(getResources().getColor(R.color.white));
-
+        handleEventAttributeCategoryChange(EventAttributeCategory.WHAT);
     }
 
     public void navigateToWhen(View v) {
-        EditText eventName = (EditText)findViewById(R.id.eventName);
-        EditText eventDescription = (EditText)findViewById(R.id.eventDescription);
-
-        eventName.setVisibility(View.GONE);
-        eventDescription.setVisibility(View.GONE);
-
-        TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
-        DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
-
-        timePicker.setVisibility(View.VISIBLE);
-        datePicker.setVisibility(View.VISIBLE);
-
-        Button what = (Button)findViewById(R.id.createEventWhatButton);
-        Button when = (Button)findViewById(R.id.createEventWhenButton);
-        what.setBackgroundColor(getResources().getColor(R.color.white));
-        when.setBackgroundColor(getResources().getColor(R.color.blue));
-
+        handleEventAttributeCategoryChange(EventAttributeCategory.WHEN);
     }
 
-    /*
-    public void navigateEventCreation(View v) {
-        EditText eventName = (EditText)findViewById(R.id.eventName);
-        EditText eventDescription = (EditText)findViewById(R.id.eventDescription);
-        eventName.setVisibility(View.GONE);
-        eventDescription.setVisibility(View.GONE);
+    public void navigateToWho(View v) {
+        handleEventAttributeCategoryChange(EventAttributeCategory.WHO);
+    }
 
-        TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
-        DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
-        timePicker.setVisibility(View.VISIBLE);
-        datePicker.setVisibility(View.VISIBLE);
+    public void navigateToWhere (View v) {
+        handleEventAttributeCategoryChange(EventAttributeCategory.WHERE);
+    }
 
+
+    private void handleEventAttributeCategoryChange(EventAttributeCategory changeTo) {
         Button what = (Button)findViewById(R.id.createEventWhatButton);
         Button when = (Button)findViewById(R.id.createEventWhenButton);
-        what.setBackgroundColor(getResources().getColor(R.color.white));
-        when.setBackgroundColor(getResources().getColor(R.color.blue));
+        Button who = (Button)findViewById(R.id.createEventWhoButton);
+        Button where = (Button)findViewById(R.id.createEventWhereButton);
 
-    }*/
+        what.setBackgroundColor(getResources().getColor(R.color.white));
+        when.setBackgroundColor(getResources().getColor(R.color.white));
+        who.setBackgroundColor(getResources().getColor(R.color.white));
+        when.setBackgroundColor(getResources().getColor(R.color.white));
+
+        CardView whatCard = (CardView)findViewById(R.id.whatView);
+        CardView whenCard = (CardView)findViewById(R.id.whenView);
+        CardView whoCard = (CardView)findViewById(R.id.whoView);
+        CardView whereCard = (CardView)findViewById(R.id.whereView);
+        whatCard.setVisibility(View.GONE);
+        whenCard.setVisibility(View.GONE);
+        whoCard.setVisibility(View.GONE);
+        whereCard.setVisibility(View.GONE);
+
+        switch (changeTo) {
+            case WHAT:
+                what.setBackgroundColor(getResources().getColor(R.color.blue));
+                whatCard.setVisibility(View.VISIBLE);
+                break;
+            case WHEN:
+                when.setBackgroundColor(getResources().getColor(R.color.blue));
+                whenCard.setVisibility(View.VISIBLE);
+                break;
+            case WHO:
+                who.setBackgroundColor(getResources().getColor(R.color.blue));
+                whoCard.setVisibility(View.VISIBLE);
+                break;
+            case WHERE:
+                where.setBackgroundColor(getResources().getColor(R.color.blue));
+                whereCard.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/edu/tufts/cs/comp150/youin/EventAttributeCategory.java
@@ -0,0 +1,12 @@
+package edu.tufts.cs.comp150.youin;
+
+/**
+ * Created by maggi on 10/3/17.
+ */
+
+public enum EventAttributeCategory {
+    WHAT,
+    WHEN,
+    WHO,
+    WHERE;
+}
