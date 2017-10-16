3a63876ee20a82a2dabf2005108bfffd7dde8338
Frank Hattler <frank.hattler@tufts.edu>
2017-10-16 14:08:34 -0400

    added real constructor for event class

--- app/src/main/java/edu/tufts/cs/comp150/youin/Event.java
+++ app/src/main/java/edu/tufts/cs/comp150/youin/Event.java
@@ -4,6 +4,7 @@ import android.widget.TextView;
 
 import java.sql.Time;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by franceshughes on 10/14/17.
@@ -13,18 +14,43 @@ public class Event {
     private String eventId;
     private String name;
     private String description;
+    private String location;
     private Date dateOfEvent;
-    private Time timeOfEvent;
+    private Time start;
+    private Time rsvpDeadline;
+    private List<String> friendsGoingIds;
+    private List<String> friendsDeclinedIds;
+    private int minPeopleToGo;
+    private String owner;
+
+
 
     public Event() {
     }
 
-    public Event(String eventId, String name, String description) {//, Date dateOfEvent, Time timeofEvent) {
+    public Event(String eventId, String name, String description) {
         this.eventId = eventId;
         this.name = name;
         this.description = description;
      //   this.dateOfEvent = dateOfEvent;
      //   this.timeOfEvent = timeofEvent;
+    }
+
+    public Event(String id, String eventName, String eventDescription, String eventLocation,
+                 Date date, Time startTime, Time respondDeadline, List<String> going,
+                 List<String> declined, int minimumPeople, String ownerId) {
+        eventId = id;
+        name = eventName;
+        description = eventDescription;
+        location = eventLocation;
+        dateOfEvent = date;
+        start = startTime;
+        rsvpDeadline = respondDeadline;
+        friendsGoingIds = going;
+        friendsDeclinedIds = declined;
+        minPeopleToGo = minimumPeople;
+        owner = ownerId;
+
     }
 
 
